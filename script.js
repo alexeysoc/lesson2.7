d38bd57a4cd56a3aeb1c8e0fbd3c2df84ba28567
Alexey <alexeysoc@rambler.ru>
2020-04-28 17:08:06 +0300

    2.10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let money=prompt('Ваш бюджет на месяц?');
-let time=prompt('Введите дату в формате YYYY-MM-DD');
+let money= +prompt('Ваш бюджет на месяц?');
+let time= prompt('Введите дату в формате YYYY-MM-DD');
 
 let appData={
     budget: money,
@@ -10,18 +10,28 @@ let appData={
     optionalExpenses: {},
     income: [],
     savings: false
+};
+
+for (let i=0; i<2; i++) {
+    let a=prompt('Введите обязательную статью расходов в этом месяце');
+    let b=prompt('Во сколько обойдется?');
+    if((typeof(a))==='string' && (typeof(a))!=null && (typeof(b))!=null
+    && a !='' && b !='' && a.length<50) {
+        console.log('done');
+        appData.expenses[a]=b;
+    } 
 }
-let a1=prompt('Введите обязательную статью расходов в этом месяце');
-let a2=prompt('Во сколько обойдется?');
-let a3=prompt('Введите обязательную статью расходов в этом месяце');
-let a4=prompt('Во сколько обойдется?');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3= a4;
+appData.moneyPerDay=appData.budget / 30;
 
-alert('бюджет на 1 день - ' + appData.budget/30);
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-console.log(appData["budget"]);
-console.log(appData["timeData"]);
-console.log(appData["expenses.a1"]);
-console.log(appData["expenses.a3"]);
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if(appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
